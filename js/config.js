d43bba1401919090635621a69cd3d4c01cc4300c
Shlokha S <shlokha2510523@ssn.edu.in>
2026-03-06 17:40:58 +0530

    switch to backend

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 
 const CONFIG = {
     MODE: "mock",   // change to "production" during final integration
-    BASE_URL: "http://localhost:5000/api"
+    BASE_URL: "https://swapshop-production.up.railway.app"
 };
